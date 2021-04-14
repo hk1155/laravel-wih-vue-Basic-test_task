cf0d08ee5f866533c23fa7abdec407122290a4e3
hk1155 <hetulpatel62@gmail.com>
2021-04-14 22:49:41 +0530

    add validation and image resizing

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
 use Illuminate\Support\Facades\Auth;
+use Validator;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class UserController extends Controller
 {
@@ -17,7 +19,7 @@ class UserController extends Controller
 
         if (Auth::attempt($credentials)) {
 
-            return response()->json(['success'=>1,'message'=>'You are Login successfully']);
+            return response()->json(['success'=>1,'message'=>'You are Login successfully','token'=>$request->bearerToken()]);
             // return redirect()->intended('dashboard');
         }
         else
@@ -28,7 +30,37 @@ class UserController extends Controller
 
     public function profile(Request $request)
     {
-        print_r($request->all());
+        // print_r($request->all());
+        // die;
+
+        // $validated = $request->validate([
+        //     'username' => 'required|max:20|min:4',
+
+        // ]);
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|max:20|min:4',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['result' => '0', "message" => "Validation error", 'errors' => $validator->errors()->messages()]);
+        }
+
+        $image       = $request->file('avtar');
+        $filename    = $image->getClientOriginalName();
+        $image_resize = Image::make($image->getRealPath());
+        $image_resize->resize(256, 256);
+        $image_resize->save(public_path('uploads/' .$filename));
+        $imagepath = public_path().'/uploads/';
+
+        // $avtar=$request->avtar;
+        // $flyername = strtolower(time().$avtar->getClientOriginalName());
+        // $flyerpath = public_path().'/uploads/';
+        // $avtar = Image::make($avtar->getRealPath());
+        // $avtar->resize(256,256);
+        // $avtar->move($flyerpath, $flyername);
+
+
+        print_r($image_resize);
         die;
     }
 }
